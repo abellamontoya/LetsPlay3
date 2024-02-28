ce49ea713d9e0d95a74f750e715fb45f02eb654a
abella <abella@elpuig.xeill.net>
2024-02-28 17:37:02 +0100

    .

---
+++ app/src/main/java/com/example/instaestepario/ContactsFragment.java
@@ -0,0 +1,28 @@
+package com.example.instaestepario;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactsFragment extends Fragment {
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+
+    }
+
+    public ContactsFragment() {
+        // Required empty public constructor
+    }
+
+
+    }
---
+++ app/src/main/java/com/example/instaestepario/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.instaestepario;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.ui.AppBarConfiguration;
+import com.example.instaestepario.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller,
+                                             @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if (destination.getId() == R.id.signInFragment) {
+                    binding.bottomNavView.setVisibility(View.GONE);
+                } else {
+                    binding.bottomNavView.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+    }
+}
