1e6c2f1c373c230fd5f352b687ef5e791a5a3ab7
abella <abella@elpuig.xeill.net>
2024-03-02 18:28:45 +0100

    .

---
+++ app/src/main/java/com/example/instaestepario/Contact.java
@@ -0,0 +1,10 @@
+package com.example.instaestepario;
+
+public class Contact {
+    public String Name;
+    public String ImageURL;
+    public Contact(String Name, String ImageURL){
+        this.ImageURL = ImageURL;
+        this.Name = Name;
+    }
+}
--- app/src/main/java/com/example/instaestepario/ContactsFragment.java
+++ app/src/main/java/com/example/instaestepario/ContactsFragment.java
@@ -4,17 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContactsFragment extends Fragment {
+    NavController navController;
+    public AppViewModel appViewModel;
+    ImageView photo;
 
     @Nullable
     @Override
@@ -23,7 +36,11 @@ public class ContactsFragment extends Fragment {
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
 
+        photo = view.findViewById(R.id.photoImageView);
+        String url = FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString();
+        Glide.with(getContext()).load(url).circleCrop().into(photo);
     }
 
     public ContactsFragment() {
